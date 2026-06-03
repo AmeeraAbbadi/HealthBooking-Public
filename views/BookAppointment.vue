c1a346638912eb3cacb95bf674993e45caf7a9c5
AmeeraAbbadi <140833828+AmeeraAbbadi@users.noreply.github.com>
2026-06-03 22:19:44 +0300

    Update BookAppointment.vue

--- views/BookAppointment.vue
+++ views/BookAppointment.vue
@@ -47,7 +47,7 @@ export default {
     };
   },
   mounted() {
-    fetch("https://gb5r23gzbb.execute-api.us-east-1.amazonaws.com/slots")
+    fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/slots")
       .then(res => res.json())
       .then(data => {
         const parsed = JSON.parse(data.body);
@@ -63,7 +63,7 @@ export default {
         slot: this.selectedSlot
       };
 
-      fetch("https://gb5r23gzbb.execute-api.us-east-1.amazonaws.com/appointments", {
+      fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/appointments", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ body: JSON.stringify(payload) })
