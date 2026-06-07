8f391365e0bcc1e42a8e962ee165be5230391606
AmeeraAbbadi <140833828+AmeeraAbbadi@users.noreply.github.com>
2026-06-07 14:06:55 +0300

    Update API endpoints for fetching slots and appointments

--- views/BookAppointment.vue
+++ views/BookAppointment.vue
@@ -47,7 +47,7 @@ export default {
     };
   },
   mounted() {
-    fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/slots")
+    fetch("https://k6yzm1znub.execute-api.eu-central-1.amazonaws.com/prod/slots")
       .then(res => res.json())
       .then(data => {
         const parsed = JSON.parse(data.body);
@@ -63,7 +63,7 @@ export default {
         slot: this.selectedSlot
       };
 
-      fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/appointments", {
+      fetch("https://k6yzm1znub.execute-api.eu-central-1.amazonaws.com/prod/appointments", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ body: JSON.stringify(payload) })
